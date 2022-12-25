e787cc2ce6b39c0877596e23f79ad726c4125334
Илья Осадчий <tapp41k@yandex.ru>
2022-12-25 23:40:36 +0300

    Debugging v2

--- homework.py
+++ homework.py
@@ -66,7 +66,7 @@ class Training:
 
     def show_training_info(self) -> InfoMessage:
         """Вернуть информационное сообщение о выполненной тренировке."""
-        return InfoMessage(type(self).__name__,
+        return InfoMessage(self.__class__.__name__,
                            self.duration,
                            self.get_distance(),
                            self.get_mean_speed(),
